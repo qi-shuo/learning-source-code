8c26f7ccc7d45a0960d9e76f15bba9b55aaad66a
qis <qi_s@qq.com>
2021-03-10 23:32:59 +0800

    不使用spring加载

--- qis-springboot/src/main/java/com/qis/springboot/init/MySpringServletContainerInitializer.java
+++ qis-springboot/src/main/java/com/qis/springboot/init/MySpringServletContainerInitializer.java
@@ -1,7 +1,5 @@
 package com.qis.springboot.init;
 
-import org.springframework.web.WebApplicationInitializer;
-
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -15,7 +13,7 @@ import java.util.Set;
  * @author qishuo
  * @date 2021/3/9 9:07 下午
  */
-@HandlesTypes({WebApplicationInitializer.class})
+@HandlesTypes({MyWebApplicationInitializer.class})
 public class MySpringServletContainerInitializer implements ServletContainerInitializer {
 
     @Override
@@ -23,21 +21,21 @@ public class MySpringServletContainerInitializer implements ServletContainerInit
         if (webAppInitializerClasses.isEmpty()) {
             return;
         }
-        List<WebApplicationInitializer> list = new ArrayList<>();
+        List<MyWebApplicationInitializer> list = new ArrayList<>();
         for (Class<?> waiClass : webAppInitializerClasses) {
             //只有不是接口和不是抽象类并且属于MyWebApplicationInitializer.class
             if (!waiClass.isInterface() &&
-                    Modifier.isAbstract(waiClass.getModifiers()) &&
+                    !Modifier.isAbstract(waiClass.getModifiers()) &&
                     //只执行自定义的MyWebApplicationInitializer
-                    MyWebApplicationInitializer.class.isAssignableFrom(waiClass)) {
+                    MyWebApplicationInitializerImpl.class.isAssignableFrom(waiClass)) {
                 try {
-                    list.add((WebApplicationInitializer) waiClass.newInstance());
+                    list.add((MyWebApplicationInitializer) waiClass.newInstance());
                 } catch (InstantiationException | IllegalAccessException e) {
                     e.printStackTrace();
                 }
             }
         }
-        for (WebApplicationInitializer webApplicationInitializer : list) {
+        for (MyWebApplicationInitializer webApplicationInitializer : list) {
             webApplicationInitializer.onStartup(servletContext);
         }
     }
--- qis-springboot/src/main/java/com/qis/springboot/init/MyWebApplicationInitializer.java
+++ qis-springboot/src/main/java/com/qis/springboot/init/MyWebApplicationInitializer.java
@@ -1,32 +1,12 @@
 package com.qis.springboot.init;
 
-import com.qis.springboot.config.AppConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 /**
  * @author qishuo
- * @date 2021/3/9 9:06 下午
+ * @date 2021/3/10 11:30 下午
  */
-public class MyWebApplicationInitializer implements WebApplicationInitializer {
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        // Load Spring web application configuration
-        //通过注解的方式初始化Spring的上下文
-        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        //注册spring的配置类（替代传统项目中xml的configuration）
-        ac.register(AppConfig.class);
-        ac.refresh();
-
-        DispatcherServlet servlet = new DispatcherServlet(ac);
-        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/app/*");
-    }
+public interface MyWebApplicationInitializer {
+    void onStartup(ServletContext servletContext) throws ServletException;
 }
