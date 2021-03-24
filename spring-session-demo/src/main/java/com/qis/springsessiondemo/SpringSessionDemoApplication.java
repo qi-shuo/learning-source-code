3150e6b07bdf2a146d511ab45cded2620f9ccf1f
qis <qi_s@qq.com>
2021-03-25 01:51:15 +0800

    使用Spring-redis-session解决session的一致性

--- spring-session-demo/src/main/java/com/qis/springsessiondemo/SpringSessionDemoApplication.java
+++ spring-session-demo/src/main/java/com/qis/springsessiondemo/SpringSessionDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
-//@EnableSpringHttpSession
+@EnableSpringHttpSession
 public class SpringSessionDemoApplication {
 
     public static void main(String[] args) {
